6d6b4bb97b57105dee56448d74c11e667b468af3
syedmuhammad12 <59411455+syedmuhammad12@users.noreply.github.com>
2022-11-10 22:16:59 +0500

    function file

---
+++ sieve.py
@@ -0,0 +1,14 @@
+def sieve(n):
+    p = [False, False] + [True]*(n-2)
+    for i in range(2,n+1):
+        for j in range(i,n+1):
+            if i*j < n:
+                p[i*j]=False
+    
+    for i in range(2,n):
+        if p[i]:
+            print(i)
+
+    print(p)
+            
+sieve(30)
